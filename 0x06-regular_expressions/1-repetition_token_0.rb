db2b58b8945f9dd15f7b12f3dcdaf562de42c1aa
fort2020 <frankfortdata@gmail.com>
2023-02-28 20:53:26 -0800

    Task 1. Fixed

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-# Find a regular expression that matches a given pattern
-puts ARGV[0],scan(/hbt{2,5}n/).join
+# Find the regular expression that matches a given pattern
+puts ARGV[0].scan(/hbt{2,5}n/).join
