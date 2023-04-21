bb9581c0249b52c7303373cf7032579fb2d9295c
fort2020 <frankfortdata@gmail.com>
2023-04-21 10:36:52 -0700

    Task 0. Fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # Using Puppet to create a file in /tmp
 
 file { '/tmp/school' :
-    ensure     => 'file',
-    permission => '0744',
-    owner      => 'www-data',
-    group      => 'www-data',
-    content    => 'I love Puppet',
+    ensure  => 'file',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
     }
