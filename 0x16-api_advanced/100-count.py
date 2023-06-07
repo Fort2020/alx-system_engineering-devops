8323614483a7b347245653a07f62e633aa02c16b
fort2020 <frankfort@gmail.com>
2023-06-06 18:15:41 -0700

    Task 3 Fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-"""a recursive function that queries the Reddit API, parses the title
-of all hot articles, and prints a sorted count of given keywords.
+"""a recursive function that queries the Reddit API, parses a itle
+of all hot articles, prints a sorted count of given keywords
 """
 import requests
 
